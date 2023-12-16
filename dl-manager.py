638d4e8e16e423497f404ac2f74e1b32316a5070
Lucca <81322668+ljb6@users.noreply.github.com>
2023-12-15 21:34:46 -0300

    Update dl-manager.py

--- dl-manager.py
+++ dl-manager.py
@@ -2,21 +2,42 @@ import os
 import mimetypes
 import shutil
 
-directory = 'DOWNLOADS_DIRECTORY'
-desktop = 'DESTINATION_FOLDER'
+# Directories paths
+DOWNLOADS_DIRECTORY = 'DOWNLOADS_DIRECTORY'
+DESTINY_DIRECTORY = 'DESTINATION_FOLDER'
 
-downloads = os.listdir(directory)
+downloads = os.listdir(DOWNLOADS_DIRECTORY)
+types = os.listdir(DESTINY_DIRECTORY)
+undefined_files = DESTINY_DIRECTORY + 'undefined-files'
 
-types = os.listdir(desktop)
-for file in os.listdir(directory):
-    file_directory = os.path.join(directory, file)
+# Select type of usage
+print('How do you want to manage your downloads?')
+move_or_copy = int(input('1 - Move\n2 - Copy/paste\n> '))
+
+if not os.path.exists(undefined_files):
+    os.makedirs(undefined_files)
+
+for file in os.listdir(DOWNLOADS_DIRECTORY):
+    file_directory = os.path.join(DOWNLOADS_DIRECTORY, file)
 
     mime, encoding = mimetypes.guess_type(file_directory)
-    
-    if mime:
-      mime = mime.split('/')
-      if mime[1] not in types:
-        types.append(mime[1])
-        os.makedirs(desktop + mime[1])
 
-      shutil.copy(file_directory, desktop + mime[1])
+    try:
+      if mime:
+        mime = mime.split('/')
+        if mime[1] not in types:
+          types.append(mime[1])
+          os.makedirs(DESTINY_DIRECTORY + mime[1])
+
+        if move_or_copy == 1:
+          shutil.move(file_directory, DESTINY_DIRECTORY + mime[1])
+        elif move_or_copy == 2:
+          shutil.copy(file_directory, DESTINY_DIRECTORY + mime[1])
+
+      else:
+        shutil.copy(file_directory, DESTINY_DIRECTORY + 'undefined-files')
+
+    except PermissionError:
+      print('File:', file, 'has not permission to move')
+
+print('Done!')
